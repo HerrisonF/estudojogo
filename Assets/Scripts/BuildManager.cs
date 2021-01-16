d0fd5b19e5d88e4709ba3374b8b5ef87343e0e09
Herrison feres <herrisonf_nascimento@hotmail.com>
2021-01-16 10:50:44 -0300

    building turrets on click

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    private GameObject turreToBuild;
+    public GameObject standardTurretPrefab;
+
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        turreToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild ()
+    {
+        return turreToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffSet;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there ! - TODO: display on screen.");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffSet, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        startColor = rend.material.color;
+        rend.material.color = hoverColor; 
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
